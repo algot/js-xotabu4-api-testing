039da843819f417cad6e14b3b6900aee0911d5ab
algot <algot@ua.fm>
2021-02-04 00:02:39 +0200

    configure local petstore container

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # HOTABU4 API testing videos
 
 https://www.youtube.com/watch?v=HmzT298UOy8
+
+
+## to run petstore container localy
+`docker pull swaggerapi/petstore`
+
+`docker run -d -e SWAGGER_HOST=http://petstore.swagger.io -e SWAGGER_URL=http://localhost  -e SWAGGER_BASE_PATH=/v2 -p 80:8080 swaggerapi/petstore`
--- tests/pet.test.ts
+++ tests/pet.test.ts
@@ -3,7 +3,7 @@ import {strict as assert} from 'assert'
 
 describe('User can', function () {
     it('receive pet by his id', async function() {
-        const response = await got('http://93.126.97.71:10080/api/pet/1')
+        const response = await got('http://localhost/v2/pet/1')
         const body = JSON.parse(response.body)
         
         assert(body.id == 1, `Expected API to return pet with id 1, but got ${body.id}`)
