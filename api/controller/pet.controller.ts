c5f2d0580b687560a8e5861840ccb8bacd9cc7a4
algot <algot@ua.fm>
2021-02-08 23:52:49 +0200

    pet tests updated

--- api/controller/pet.controller.ts
+++ api/controller/pet.controller.ts
@@ -22,4 +22,52 @@ export class PetController {
         })
         return JSON.parse(response.body)
     }
+
+    async addNew(pet: {
+        category: {
+            id: number;
+            name: string;
+        };
+        name: string;
+        photoUrls: string[];
+        tags: {
+            id: number;
+            name: string;
+        }[];
+        status: string,
+    }) {
+        const response = await got(`${host}/pet`, {
+            method: 'POST',
+            json: pet
+        });
+        return JSON.parse(response.body);
+    }
+
+    async delete(id: number | string) {
+        const response = await got(`${host}/pet/${id}`, {
+            method: 'DELETE'
+        })
+        return JSON.parse(response.body)
+    }
+
+    async update(pet: {
+        id: number,
+        category: {
+            id: number,
+            name: string,
+        },
+        name: string,
+        photoUrls: string[],
+        tags: {
+            id: number,
+            name: string,
+        }[],
+        status: string,
+    }) {
+        const response = await got(`${host}/pet`, {
+            method: 'PUT',
+            json: pet
+        })
+        return JSON.parse(response.body)
+    }
 }
