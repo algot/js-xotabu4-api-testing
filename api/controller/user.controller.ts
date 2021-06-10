5db16c5bd903dc871877a2af0c98ff5be16b51eb
algot <algot@ua.fm>
2021-06-10 12:44:04 +0300

    Added additional test before video Part8 - adding allure report

--- api/controller/user.controller.ts
+++ api/controller/user.controller.ts
@@ -1,7 +1,15 @@
-import { operations } from "../../.temp/types";
+import { definitions, operations } from "../../.temp/types";
 import { BaseController } from "./base.controller";
 
 export class UserController extends BaseController {
+    async register(userToCreate: Omit<definitions['User'], 'id' | 'userStatus'>) {
+        return (await this.request()
+            .url('user/register')
+            .method('POST')
+            .body(userToCreate)
+            .send<operations['registerUser']['responses']['200']['schema']>()
+        ).body
+    }
     async login(credentials: { username: string, password: string }) {
         return (await this.request()
             .url('user/login')
